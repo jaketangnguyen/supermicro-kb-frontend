06cb7f101a4543168060d4d4dc59841cf7d89ef8
Jake Nguyen <jaketangnguyen@gmail.com>
2022-07-19 14:44:00 -0700

    Initial commit

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
                 <nav className="nav">
                     <img className="logo" src={require("./Photos/smc.png")} />
                     <ul className="nav-items">
-                        <li className="nav-link"><a href="#">Product Families123</a></li>
+                        <li className="nav-link"><a href="#">Product Families</a></li>
                         <li className="nav-link"><a href="#">Components</a></li>
                         <li className="nav-link"><a href="#">Common Questions</a></li>
                     </ul>
