b906211e51cd5d3914c9a7a68e1947577d84fadd
Jake Nguyen <jaketangnguyen@gmail.com>
2022-07-20 11:17:25 -0700

    Add site-id and change styling

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,18 @@ import React from "react"
 
 export default function Header() {
     return (
-            <header className="header">
-                <nav className="nav">
+        <header className="header">
+            <nav className="nav">
+                <div className="brand">
                     <img className="logo" src={require("./Photos/smc.png")} />
-                    <ul className="nav-items">
-                        <li className="nav-link"><a href="#">Product Families</a></li>
-                        <li className="nav-link"><a href="#">Components</a></li>
-                        <li className="nav-link"><a href="#">Common Questions</a></li>
-                    </ul>
-                </nav>
-            </header>
+                    <div className="site-id">Knowledge Base</div>
+                </div>
+                <ul className="nav-items">
+                    <li className="nav-link"><a href="./families.html">Product Families</a></li>
+                    <li className="nav-link"><a href="#">Components</a></li>
+                    <li className="nav-link"><a href="#">Common Questions</a></li>
+                </ul>
+            </nav>
+        </header>
     )
   }
