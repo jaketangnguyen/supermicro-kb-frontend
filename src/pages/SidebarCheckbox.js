23a5414709be44228f19e09bff115e6ba7045bce
EDiT0330 <ethan.tu246@gmail.com>
2023-01-09 01:17:54 -0800

    hard coded sections for filters

--- src/pages/SidebarCheckbox.js
+++ src/pages/SidebarCheckbox.js
@@ -18,35 +18,87 @@ class SidebarCheckbox extends React.Component {
 
   render() {
     return (
-      <Tab.Container>
-        <Row>
-          <Col sm={4}>
+      <div>
+        <Tab.Container>
+          <Row>
+            <Col sm={4}>
 
-            <ListGroup className="sidebar">
-              
-              <header className="sBarCat">
-                {'Categories'}
-              </header>
+              <ListGroup className="sidebar">
 
-              {this.props.categories.map(category => (
+                <header className="sBarCat">
+                  {'Categories'}
+                </header>
 
-                <ListGroup.Item className="filterList" key={category}>
-                  <label>
+                <ListGroup>
+                  <ListGroup.Item >
+                    <label>
+                      <input type="checkbox" /> {'Questions & Answers'}
+                    </label>
+                  </ListGroup.Item>
+                  <ListGroup.Item >
+                    <label>
+                      <input type="checkbox" /> {'Articles'}
+                    </label>
+                  </ListGroup.Item>
+                  <ListGroup.Item >
+                    <label>
+                      <input type="checkbox" /> {'Others'}
+                    </label>
+                  </ListGroup.Item>
+                </ListGroup>
 
-                    <input
-                      onChange={this.handleChange}
-                      type="checkbox"
-                      value={category} />
+                <ListGroup>
 
-                    {category}
+                  <header className="sBarCat">
+                    {'Components'}
+                  </header>
+                  
+                  <ListGroup.Item >
+                    <input type="checkbox" /> {'System'}
+                  </ListGroup.Item>
+                  <ListGroup.Item >
+                    <input type="checkbox" /> {'CPU'}
+                  </ListGroup.Item>
+                  <ListGroup.Item >
+                    <input type="checkbox" /> {'Memory'}
+                  </ListGroup.Item>
+                  <ListGroup.Item >
+                    <input type="checkbox" /> {'AOC'}
+                  </ListGroup.Item>
+                  <ListGroup.Item >
+                    <input type="checkbox" /> {'GPU'}
+                  </ListGroup.Item>
+                  <ListGroup.Item >
+                    <input type="checkbox" /> {'Network'}
+                  </ListGroup.Item>
+                  <ListGroup.Item >
+                    <input type="checkbox" /> {'Storage Controller'}
+                  </ListGroup.Item>
+                  <ListGroup.Item >
+                    <input type="checkbox" /> {'category'}
+                  </ListGroup.Item>
+                </ListGroup>
+                {/* {this.props.categories.map(category => (
 
-                  </label>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          </Col>
-        </Row>
-      </Tab.Container>
+                  <ListGroup.Item className="filterList" key={category}>
+                    <label>
+
+                      <input
+                        onChange={this.handleChange}
+                        type="checkbox"
+                        value={category} />
+
+                      {category}
+
+                    </label>
+                  </ListGroup.Item>
+                ))} */}
+
+              </ListGroup>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </div>
     );
   };
 }
